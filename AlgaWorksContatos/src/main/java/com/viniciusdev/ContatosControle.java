70c5a5b9a6a414455926d82fbdcd84c70b7ef222
Morcego <vinicius.templario@gmail.com>
2023-06-25 21:44:49 -0300

    Listando os contatos

--- AlgaWorksContatos/src/main/java/com/viniciusdev/ContatosControle.java
+++ AlgaWorksContatos/src/main/java/com/viniciusdev/ContatosControle.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ContatosControle {
@@ -13,10 +14,10 @@ public class ContatosControle {
 	
 	static {
 		LISTA_CONTATOS.addAll(Arrays.asList(
-					new Contato("1", "Joao", "+55 00 00000-0000"),
-					new Contato("2", "Maria Cecilia", "+55 11 11111-1111"),
-					new Contato("3", "Cristiane", "+55 33 33333-3333"),
-					new Contato("4", "Vinicius", "+55 44 44444-4444")
+					new Contato("1", "Joao", "+55 34 00000-0000"),
+					new Contato("2", "Maria Cecilia", "+55 34 11111-1111"),
+					new Contato("3", "Cristiane", "+55 34 33333-3333"),
+					new Contato("4", "Vinicius", "+55 34 44444-4444")
 				));
 		
 	}
@@ -24,5 +25,16 @@ public class ContatosControle {
 	@GetMapping("/")
 	public String index() {	
 		return "index";
+	}
+	
+	@GetMapping("/contatos")
+	public ModelAndView listar() {
+		ModelAndView modelAndView = new ModelAndView("listar");
+		modelAndView.addObject("contatos", LISTA_CONTATOS);
+		return modelAndView;
+	}
+	@GetMapping("/contatos/novo")
+	public String novo() {
+		return "formulario";
 	}
 }
